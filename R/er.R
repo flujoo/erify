87acaf743284f254cf3fb0c2b90936df3c3bbf8b
flujoo <tnerrotana@gmail.com>
2021-04-03 20:28:04 +0400

    Add `join`

--- R/er.R
+++ R/er.R
@@ -3,3 +3,18 @@ NULL
 
 
 utils::globalVariables(".")
+
+
+join <- function(words, conjunction = "or") {
+  l <- length(words)
+
+  if (l == 1) {
+    return(words)
+  }
+
+  paste(
+    paste(words[-l], collapse = ", "),
+    conjunction,
+    words[l]
+  )
+}
