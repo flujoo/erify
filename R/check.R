97046d060df3edc909e9a59f7f63276ac51081c0
flujoo <tnerrotana@gmail.com>
2021-04-06 11:57:03 +0400

    Organize code in check.R

--- R/check.R
+++ R/check.R
@@ -1,283 +1,3 @@
-# internal ----------------------------------------------------------------
-
-.check_type <- function(x, valid, name = NULL, general = NULL,
-                        specifics = NULL, supplement = NULL, ...) {
-  type <- typeof(x)
-
-  if (type %in% valid) {
-    return(invisible(NULL))
-  }
-
-  if (is.null(name)) {
-    name <- deparse(substitute(x))
-    name <- glue("`{name}`")
-  }
-
-  valid %<>% join()
-
-  if (is.null(general)) {
-    general <- "{name} must have type {valid}."
-  }
-
-  if (is.null(specifics)) {
-    specifics = "{name} has type {type}."
-  }
-
-  .Statement(general, specifics, supplement, env = environment(), ...) %>%
-    .trigger()
-}
-
-
-.check_length <- function(x, valid, interval = NULL, name = NULL,
-                          general = NULL, specifics = NULL,
-                          supplement = NULL, ...) {
-  l <- length(x)
-  interval %<>% normalize_interval(valid)
-
-  if (is_valid_length(l, valid, interval)) {
-    return(invisible(NULL))
-  }
-
-  if (is.null(name)) {
-    name <- deparse(substitute(x))
-    name <- glue("`{name}`")
-  }
-
-  valid %<>% phrase_valid_length(interval)
-
-  if (is.null(general)) {
-    general <- "{name} must have length {valid}."
-  }
-
-  if (is.null(specifics)) {
-    specifics = "{name} has length {l}."
-  }
-
-  .Statement(general, specifics, supplement, env = environment(), ...) %>%
-    .trigger()
-}
-
-
-.check_in <- function(x, valid, name = NULL, general = NULL,
-                      specifics = NULL, supplement = NULL, ...) {
-  if (x %in% valid) {
-    return(invisible(NULL))
-  }
-
-  if (is.null(name)) {
-    name <- deparse(substitute(x))
-    name <- glue("`{name}`")
-  }
-
-  # treat integers as doubles
-  if (is.integer(x)) {
-    x %<>% as.double()
-  }
-
-  if (is.integer(valid)) {
-    valid %<>% as.double()
-  }
-
-  valid %<>%
-    as_code(TRUE) %>%
-    join()
-
-  x %<>% as_code(env = list(x = x))
-
-  if (is.null(general)) {
-    general <- "{name} must be {valid}."
-  }
-
-  if (is.null(specifics)) {
-    specifics = "{name} is {x}."
-  }
-
-  .Statement(general, specifics, supplement, env = environment(), ...) %>%
-    .trigger()
-}
-
-
-.check_single_character <- function(x, name = NULL, general = NULL,
-                                    specifics = NULL, supplement = NULL,
-                                    ...) {
-  if (is.null(name)) {
-    name <- deparse(substitute(x))
-    name <- glue("`{name}`")
-  }
-
-  if (is.null(general)) {
-    general <- "{name} must be a single character."
-  }
-
-  .check_type(x, "character", name, general, specifics, supplement, ...)
-  .check_length(x, 1, NULL, name, general, specifics, supplement, ...)
-}
-
-
-
-# utils -------------------------------------------------------------------
-
-normalize_interval <- function(interval, valid) {
-  if (!is.null(interval)) {
-    return(interval)
-  }
-
-  l <- length(valid)
-
-  if (l != 2) {
-    FALSE
-
-  } else {
-    con <- valid[2] > (valid[1] + 1)
-    ifelse(is.na(con), TRUE, con)
-  }
-}
-
-
-is_valid_length <- function(l, valid, interval) {
-  if (!interval) {
-    l %in% valid
-
-  } else {
-    con <- l > valid[1] && l < valid[2]
-    ifelse(is.na(con), TRUE, con)
-  }
-}
-
-
-phrase_valid_length <- function(valid, interval) {
-  if (!interval) {
-    join(valid)
-
-  } else {
-    v1 <- valid[1]
-    v2 <- valid[2]
-    ss <- character(0)
-
-    if (!is.na(v1)) {
-      ss %<>% c(glue("larger than {v1}"))
-    }
-
-    if (!is.na(v2)) {
-      ss %<>% c(glue("smaller than {v2}"))
-    }
-
-    join(ss, "and")
-  }
-}
-
-
-# this is black magic ...
-as_code <- function(x, recursive = FALSE, env = environment()) {
-  if (!recursive) {
-    s <- deparse(substitute(x, env = env))
-    # can't use %>% here
-
-  } else {
-    s <- character(0)
-
-    for (x_i in x) {
-      s_i <- deparse(substitute(x_i))
-      s %<>% c(s_i)
-    }
-  }
-
-  glue("`{s}`")
-}
-
-
-check_error <- function(name, general, specifics, supplement) {
-  if (!is.null(name)) {
-    .check_single_character(name)
-  }
-
-  if (!is.null(general)) {
-    .check_single_character(general)
-  }
-
-  if (!is.null(specifics)) {
-    .check_type(specifics, "character")
-  }
-
-  if (!is.null(supplement)) {
-    .check_single_character(supplement)
-  }
-}
-
-
-is_integer <- function(x) {
-  con <- is.numeric(x) && all(as.integer(x) == x)
-  ifelse(is.na(con), TRUE, con)
-}
-
-
-check_length_valid <- function(valid, interval) {
-  #
-  general <- "`valid` must be a numeric vector of non-negative integers."
-
-  .check_type(valid, c("integer", "double"), general = general)
-  .check_length(valid, c(0, NA), general = general)
-
-  specific <- "valid[{i}] is {v}."
-  specifics <- character(0)
-
-  for (i in 1:length(valid)) {
-    v <- valid[i]
-    con <- is_integer(v) && (is.na(v) || v >= 0)
-
-    if (!con) {
-      specifics %<>% c(glue(specific))
-    }
-  }
-
-  if (length(specifics) > 0) {
-    .Statement(general, specifics) %>% .trigger()
-  }
-
-  #
-  if (isFALSE(interval)) {
-    if (all(is.na(valid))) {
-      general <-
-        "If `interval` is `FALSE`, `valid` must not contain only `NA`."
-      specifics <-
-        "`valid` is {valid}."
-
-      valid %<>% as_code(env = list(x = valid))
-
-      .Statement(general, specifics, env = environment()) %>%
-        .trigger()
-    }
-  }
-
-  if (isTRUE(interval)) {
-    general <- "If `interval` is `TRUE`, `valid` must have length 2."
-    .check_length(valid, 2, general = general)
-
-    con <- valid[2] - 1 > valid[1]
-    con <- ifelse(is.na(con), TRUE, con)
-
-    if (con) {
-      return(invisible(NULL))
-    }
-
-    general <- paste(
-      "If `interval` is `TRUE`,",
-      "`valid[2]` must be larger than `valid[1] + 1,",
-      "if both are not `NA`."
-    )
-
-    specifics <-
-      "`valid` is {valid}."
-
-    valid %<>% as_code(env = list(x = valid))
-
-    .Statement(general, specifics, env = environment()) %>%
-      .trigger()
-  }
-}
-
-
-
 # dummy -------------------------------------------------------------------
 
 #' @name check_argument
@@ -385,7 +105,35 @@ NULL
 
 
 
-# exported ----------------------------------------------------------------
+# type --------------------------------------------------------------------
+
+.check_type <- function(x, valid, name = NULL, general = NULL,
+                        specifics = NULL, supplement = NULL, ...) {
+  type <- typeof(x)
+
+  if (type %in% valid) {
+    return(invisible(NULL))
+  }
+
+  if (is.null(name)) {
+    name <- deparse(substitute(x))
+    name <- glue("`{name}`")
+  }
+
+  valid %<>% join()
+
+  if (is.null(general)) {
+    general <- "{name} must have type {valid}."
+  }
+
+  if (is.null(specifics)) {
+    specifics = "{name} has type {type}."
+  }
+
+  .Statement(general, specifics, supplement, env = environment(), ...) %>%
+    .trigger()
+}
+
 
 #' @rdname check_argument
 #' @export
@@ -438,6 +186,89 @@ check_class <- function(x, valid, name = NULL, general = NULL,
 }
 
 
+
+# length ------------------------------------------------------------------
+
+.check_length <- function(x, valid, interval = NULL, name = NULL,
+                          general = NULL, specifics = NULL,
+                          supplement = NULL, ...) {
+  l <- length(x)
+  interval %<>% normalize_interval(valid)
+
+  if (is_valid_length(l, valid, interval)) {
+    return(invisible(NULL))
+  }
+
+  if (is.null(name)) {
+    name <- deparse(substitute(x))
+    name <- glue("`{name}`")
+  }
+
+  valid %<>% phrase_valid_length(interval)
+
+  if (is.null(general)) {
+    general <- "{name} must have length {valid}."
+  }
+
+  if (is.null(specifics)) {
+    specifics = "{name} has length {l}."
+  }
+
+  .Statement(general, specifics, supplement, env = environment(), ...) %>%
+    .trigger()
+}
+
+
+normalize_interval <- function(interval, valid) {
+  if (!is.null(interval)) {
+    return(interval)
+  }
+
+  l <- length(valid)
+
+  if (l != 2) {
+    FALSE
+
+  } else {
+    con <- valid[2] > (valid[1] + 1)
+    ifelse(is.na(con), TRUE, con)
+  }
+}
+
+
+is_valid_length <- function(l, valid, interval) {
+  if (!interval) {
+    l %in% valid
+
+  } else {
+    con <- l > valid[1] && l < valid[2]
+    ifelse(is.na(con), TRUE, con)
+  }
+}
+
+
+phrase_valid_length <- function(valid, interval) {
+  if (!interval) {
+    join(valid)
+
+  } else {
+    v1 <- valid[1]
+    v2 <- valid[2]
+    ss <- character(0)
+
+    if (!is.na(v1)) {
+      ss %<>% c(glue("larger than {v1}"))
+    }
+
+    if (!is.na(v2)) {
+      ss %<>% c(glue("smaller than {v2}"))
+    }
+
+    join(ss, "and")
+  }
+}
+
+
 #' @rdname check_argument
 #' @export
 check_length <- function(x, valid, interval = NULL, name = NULL,
@@ -458,4 +289,179 @@ check_length <- function(x, valid, interval = NULL, name = NULL,
 
   .check_length(
     x, valid, interval, name, general, specifics, supplement, ...)
+}
+
+
+is_integer <- function(x) {
+  con <- is.numeric(x) && all(as.integer(x) == x)
+  ifelse(is.na(con), TRUE, con)
+}
+
+
+check_length_valid <- function(valid, interval) {
+  #
+  general <- "`valid` must be a numeric vector of non-negative integers."
+
+  .check_type(valid, c("integer", "double"), general = general)
+  .check_length(valid, c(0, NA), general = general)
+
+  specific <- "valid[{i}] is {v}."
+  specifics <- character(0)
+
+  for (i in 1:length(valid)) {
+    v <- valid[i]
+    con <- is_integer(v) && (is.na(v) || v >= 0)
+
+    if (!con) {
+      specifics %<>% c(glue(specific))
+    }
+  }
+
+  if (length(specifics) > 0) {
+    .Statement(general, specifics) %>% .trigger()
+  }
+
+  #
+  if (isFALSE(interval)) {
+    if (all(is.na(valid))) {
+      general <-
+        "If `interval` is `FALSE`, `valid` must not contain only `NA`."
+      specifics <-
+        "`valid` is {valid}."
+
+      valid %<>% as_code(env = list(x = valid))
+
+      .Statement(general, specifics, env = environment()) %>%
+        .trigger()
+    }
+  }
+
+  if (isTRUE(interval)) {
+    general <- "If `interval` is `TRUE`, `valid` must have length 2."
+    .check_length(valid, 2, general = general)
+
+    con <- valid[2] - 1 > valid[1]
+    con <- ifelse(is.na(con), TRUE, con)
+
+    if (con) {
+      return(invisible(NULL))
+    }
+
+    general <- paste(
+      "If `interval` is `TRUE`,",
+      "`valid[2]` must be larger than `valid[1] + 1,",
+      "if both are not `NA`."
+    )
+
+    specifics <-
+      "`valid` is {valid}."
+
+    valid %<>% as_code(env = list(x = valid))
+
+    .Statement(general, specifics, env = environment()) %>%
+      .trigger()
+  }
+}
+
+
+
+# in ----------------------------------------------------------------------
+
+.check_in <- function(x, valid, name = NULL, general = NULL,
+                      specifics = NULL, supplement = NULL, ...) {
+  if (x %in% valid) {
+    return(invisible(NULL))
+  }
+
+  if (is.null(name)) {
+    name <- deparse(substitute(x))
+    name <- glue("`{name}`")
+  }
+
+  # treat integers as doubles
+  if (is.integer(x)) {
+    x %<>% as.double()
+  }
+
+  if (is.integer(valid)) {
+    valid %<>% as.double()
+  }
+
+  valid %<>%
+    as_code(TRUE) %>%
+    join()
+
+  x %<>% as_code(env = list(x = x))
+
+  if (is.null(general)) {
+    general <- "{name} must be {valid}."
+  }
+
+  if (is.null(specifics)) {
+    specifics = "{name} is {x}."
+  }
+
+  .Statement(general, specifics, supplement, env = environment(), ...) %>%
+    .trigger()
+}
+
+
+
+# single character --------------------------------------------------------
+
+.check_single_character <- function(x, name = NULL, general = NULL,
+                                    specifics = NULL, supplement = NULL,
+                                    ...) {
+  if (is.null(name)) {
+    name <- deparse(substitute(x))
+    name <- glue("`{name}`")
+  }
+
+  if (is.null(general)) {
+    general <- "{name} must be a single character."
+  }
+
+  .check_type(x, "character", name, general, specifics, supplement, ...)
+  .check_length(x, 1, NULL, name, general, specifics, supplement, ...)
+}
+
+
+
+# utils -------------------------------------------------------------------
+
+# this is black magic ...
+as_code <- function(x, recursive = FALSE, env = environment()) {
+  if (!recursive) {
+    s <- deparse(substitute(x, env = env))
+    # can't use %>% here
+
+  } else {
+    s <- character(0)
+
+    for (x_i in x) {
+      s_i <- deparse(substitute(x_i))
+      s %<>% c(s_i)
+    }
+  }
+
+  glue("`{s}`")
+}
+
+
+check_error <- function(name, general, specifics, supplement) {
+  if (!is.null(name)) {
+    .check_single_character(name)
+  }
+
+  if (!is.null(general)) {
+    .check_single_character(general)
+  }
+
+  if (!is.null(specifics)) {
+    .check_type(specifics, "character")
+  }
+
+  if (!is.null(supplement)) {
+    .check_single_character(supplement)
+  }
 }
