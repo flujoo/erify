03bf0bf3862bd871ffcc8b344586b59fdad72bba
flujoo <tnerrotana@gmail.com>
2021-04-13 22:30:39 +0400

    Add `Statement`

--- R/statement.R
+++ R/statement.R
@@ -134,3 +134,114 @@ is_empty <- function(statement) {
 
   do.call(f, args)
 }
+
+
+
+# Statement ---------------------------------------------------------------
+
+#' @title Create `Statement` Object
+#'
+#' @description Create a `Statement` object.
+#'
+#' `Statement` objects are structured representations of
+#' normal, warning or error messages.
+#'
+#' @param general A single character which represents the general statement
+#' of a message.
+#'
+#' @param specifics Optional. A character vector which represents the details
+#' of a message. If `specifics` is a named vector, the names are used to
+#' create bullets. if the name is `"x"` or `"i"`, the bullet will be colored
+#' and bold. Any item with no name will be named with `"x"`. Argument
+#' `decorate` is used to turn on/off this process of adding and decorating
+#' bullets. See "Examples" section.
+#'
+#' @param supplement Optional. A single character which represents the
+#' supplementary message appended at the end.
+#'
+#' @param env Optional. An environment or named list which is used
+#' to evaluate the R code in the above three arguments.
+#' See "Examples" section and [glue::glue()].
+#'
+#' @param decorate Optional. `TRUE` or `FALSE` which indicates if to decorate
+#' the bullets of `specifics`. The default value is `TRUE`.
+#'
+#' @param ... Optional. Additional arguments which can be passed to
+#' [rlang::abort()] or related functions.
+#'
+#' @return A list of class `Statement`.
+#'
+#' @seealso [trigger()] for generating normal, warning and error messages.
+#'
+#' [glue::glue()] for inserting R code into characters.
+#'
+#' [rlang::abort()] for adding additional arguments.
+#'
+#' `vignette("er", package = "er")` for a gentle introduction to this package.
+#'
+#' [The tidyverse style guide](https://style.tidyverse.org/error-messages.html)
+#' for more details about the style behind `Statement` objects.
+#'
+#' @examples
+#' # quick example
+#' general <- "I am the general statement of the message."
+#' specifics <- c("Detail 1.", i = "Detail 2.", `*` = "Detail 3")
+#' supplement <- "I am the additional information."
+#' Statement(general, specifics, supplement)
+#'
+#' # do not decorate bullets
+#' Statement(general, specifics, supplement, decorate = FALSE)
+#'
+#' # use R code in message
+#' Statement("`x` is `{x}`.", env = list(x = 1))
+#' @export
+Statement <- function(general, specifics = NULL, supplement = NULL,
+                      env = NULL, decorate = NULL, ...) {
+  check_statement(NULL, general, specifics, supplement)
+  check_env(env)
+
+  if (!is.null(decorate)) {
+    check_bool(decorate)
+  }
+
+  if (is.null(decorate)) {
+    decorate <- TRUE
+  }
+
+  .Statement(general, specifics, supplement, env, decorate, ...)
+}
+
+
+check_env <- function(env) {
+  if (is.null(env)) {
+    return(invisible(NULL))
+  }
+
+  check_type(env, c("environment", "list"))
+
+  if (is.list(env)) {
+    general <- "If `env` is list, each item of it must be named."
+    ns <- names(env)
+
+    if (length(env) == 0) {
+      return(invisible(NULL))
+
+    } else if (is.null(ns)) {
+      .Statement(general, "`names(env)` is `NULL`.") %>% .trigger()
+
+    } else {
+      specifics <- character(0)
+      specific <- "`env[[{i}]]` has no name."
+
+      for (i in 1:length(ns)) {
+        if (ns[i] == "") {
+          specifics %<>% c(glue(specific))
+        }
+      }
+
+      if (length(specifics) != 0) {
+        .Statement(general, specifics) %>% .trigger()
+      }
+    }
+  }
+}
