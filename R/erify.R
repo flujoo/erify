e32fc7c4053378ccf82928dc743fa5f3bcd42346
flujoo <tnerrotana@gmail.com>
2021-04-22 13:58:57 +0400

    Add `check_arguments`

--- R/erify.R
+++ R/erify.R
@@ -25,3 +25,32 @@ glue <- function(x, env = parent.frame()) {
     glue::glue(.envir = env) %>%
     unclass()
 }
+
+
+# shortcut to check commonly used arguments
+check_arguments <- function(name = NULL, general = NULL, specifics = NULL,
+                            supplement = NULL, specific = NULL, n = NULL) {
+  if (!is.null(name)) {
+    .check_string(name)
+  }
+
+  if (!is.null(general)) {
+    .check_string(general)
+  }
+
+  if (!is.null(specifics)) {
+    .check_type(specifics, "character")
+  }
+
+  if (!is.null(supplement)) {
+    .check_string(supplement)
+  }
+
+  if (!is.null(specific)) {
+    .check_string(specific)
+  }
+
+  if (!is.null(n)) {
+    check_index(n)
+  }
+}
