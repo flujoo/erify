2ea6dce4352febead411adeee4b8ba9b51ed0e4b
flujoo <tnerrotana@gmail.com>
2021-04-22 18:04:00 +0400

    Document `check_types`

--- R/type.R
+++ R/type.R
@@ -101,9 +101,55 @@ check_type <- function(x, valid, name = NULL, general = NULL,
 }
 
 
-#' @rdname validators
-#' @order 3
+#' @inherit check_type
+#'
+#' @title Check Each Item's Type
+#'
+#' @description Check if each item of an argument has valid type,
+#' and if not, generate an error message.
+#'
+#' @param x The argument to check, which must be a list.
+#'
+#' @param specific Optional. A single character which gives the template for
+#' reporting each invalid item. R expressions can be inserted with
+#' [glue::glue()] syntax. See "Details" section for more details. By default,
+#' this argument is generated automatically.
+#'
+#' @param n Optional. A positive integer which indicates how many invalid
+#' items at most to display. The default value is `5`.
+#'
+#' @details The R expressions in argument `specific` can contain
+#'
+#' - argument `name`,
+#' - argument `i` which indicates the index of an invalid item, and
+#' - argument `type` which indicates the item's type.
+#'
+#' @seealso "Examples" section in [check_type()] for how to customize error
+#' message and how to add additional arguments.
+#'
+#' [glue::glue()] for inserting R expressions into characters.
+#'
+#' @template seealso
+#'
 #' @export
+#'
+#' @examples
+#' \dontrun{
+#' # argument to check
+#' arg <- as.list(1:10)
+#'
+#' check_types(arg, "character")
+#'
+#' # specify argument `specific`
+#' check_types(
+#'   arg,
+#'   "character",
+#'   specific = "`{name}[[{i}]]` is an {type}, oh my god!"
+#' )
+#'
+#' # change argument `n`
+#' check_types(arg, "character", n = 3)
+#' }
 check_types <- function(x, valid, name = NULL, general = NULL,
                         specific = NULL, supplement = NULL, n = NULL, ...) {
   # check arguments
