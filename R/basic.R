7c617ff73c202e84a668e4698e38b036de632333
flujoo <tnerrotana@gmail.com>
2021-04-25 18:34:16 +0400

    Modify `check_content_valid`

--- R/basic.R
+++ R/basic.R
@@ -87,31 +87,16 @@ check_type <- function(x, valid, name = NULL, general = NULL,
 # content -----------------------------------------------------------------
 
 check_content_valid <- function(valid) {
-  pre <- getOption("erify.prepend")
-
   # check type
-  pass <- (is.atomic(valid) && !is.null(valid)) ||
-    is.function(valid) ||
-    is.expression(valid)
+  pass <- is.atomic(valid) && !is.null(valid)
 
   if (!pass) {
-    general <- paste(
-      pre,
-      "`valid` must be a function, an expression, or an atomic vector."
-    )
-
+    pre <- getOption("erify.prepend")
+    general <- paste(pre, "`valid` must have an atomic type.")
     specific <- "`valid` has type { typeof(valid) }."
-
     .Statement(general, specific, environment()) %>% .trigger()
   }
 
   # check length
-  if (is.atomic(valid)) {
-    general <- paste(
-      pre,
-      "If `valid` is atomic, it must have length larger than 0."
-    )
-
-    .check_length(valid, c(0, NA), general = general)
-  }
+  .check_length(valid, c(0, NA), general = getOption("erify.general"))
 }
