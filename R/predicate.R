848940ab046f597f411fcc215f661016982b4aa6
flujoo <tnerrotana@gmail.com>
2021-04-25 21:10:11 +0400

    Revise `is_integer`

--- R/predicate.R
+++ R/predicate.R
@@ -1,12 +1,7 @@
-is_integer <- function(x, na = NULL) {
-  if (!is.null(na)) {
-    # check_bool(na)
-  }
-
-  if (is.null(na)) {
-    na <- TRUE
-  }
-
-  con <- is.numeric(x) && all(as.integer(x) == x)
-  ifelse(is.na(con), na, con)
+is_integer <- function(x) {
+  all(
+    is.numeric(x),
+    is.finite(x), # also excludes `NA`
+    all(as.integer(x) == x)
+  )
 }
