bc4be4b3c9d604db45a6e681afe9c45a028fbaf2
flujoo <tnerrotana@gmail.com>
2021-04-26 20:14:01 +0400

    Fix bug in `n` in `check_arguments`

--- R/internal.R
+++ R/internal.R
@@ -69,6 +69,8 @@ check_arguments <- function(name = NULL, general = NULL, specific = NULL,
   }
 
   if (!is.null(n)) {
+    pre <- getOption("erify.prepend")
+    g <- paste(pre, "`{name}` must be a single positive integer.")
     check_index(n, general = g)
   }
 }
